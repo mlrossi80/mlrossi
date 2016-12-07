46385d1c26e09cdc53b4226e8930c5f7a69a9178
Alex Peck <alex@alxpck.com>
2016-12-07 16:56:05 -0500

    Add CV page to the site

---
+++ cv.php
@@ -0,0 +1,222 @@
+<?php 
+// $ogimage = '';
+$page = 'cv';
+$title = 'Curriculum Vitae';
+include('header.php'); 
+?>
+
+<div class="container logoblock"> 
+	<div class="row">
+
+		<!-- MOBILE -->
+		<div class="visible-xs-block col-xs-12">
+			<div class="row">
+
+				<div class="col-xs-12">
+					<h2><p class="lead" style="margin-top:-25px; margin-bottom:-50px">Curriculum Vitae</p></h2>
+				</div>
+
+			</div>
+		</div>
+		
+		<!-- DESKTOP -->
+		
+
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+
+			<p class="hidden-xs lead">Curriculum Vitae</p>
+
+			<div class="row bio">
+				<!-- MOBILE -->
+			
+
+				<!-- DESKTOP -->
+				<div class="hidden-xs col-sm-4">
+					<img class="img-responsive" style="padding-top: 5px;" src="img/Matthew_Rossi_640.jpg" height="175" width="175" />
+				</div>
+
+				<!-- MOBILE -->
+				<div class="visible-xs col-xs-12">
+					<p><strong>Matthew Louis Rossi</strong><br/><a href='ma&#105;lto&#58;m&#108;%7&#50;%6Fss&#105;&#56;0&#64;%&#54;7m%61%6&#57;l&#46;c&#111;&#109;'>mlro&#115;si8&#48;&#64;gmai&#108;&#46;&#99;&#111;&#109;</a><br/>773.787.7245</p>
+
+					<!-- Email obfuscated to avoid spam -->
+					<!-- http://www.albionresearch.com/misc/obfuscator.php -->
+
+				
+
+				</div>
+
+				<!-- DESKTOP -->
+				<div class="hidden-xs col-sm-8">
+					<p><strong>Matthew Louis Rossi</strong><br/><a href='ma&#105;lto&#58;m&#108;%7&#50;%6Fss&#105;&#56;0&#64;%&#54;7m%61%6&#57;l&#46;c&#111;&#109;'>mlro&#115;si8&#48;&#64;gmai&#108;&#46;&#99;&#111;&#109;</a><br/>773.787.7245</p>
+
+					<!-- Email obfuscated to avoid spam -->
+					<!-- http://www.albionresearch.com/misc/obfuscator.php -->
+
+					
+
+				</div>
+			</div> <!-- end bio -->			
+			
+			<!-- Bootstrap description list for CV -->
+			<!-- http://getbootstrap.com/css/#description -->
+			<dl>
+  				
+  				<dt>PUBLICATIONS</dt>
+  				<dd>“The Private Eye,” Gigantic Worlds. Ed. Lincoln Michel: Gigantic Books; November 2015.</dd>
+  				<dd>“The Bee, Ha-Shem,” West Branch. Summer 2014, Vol. 75; August 2014</dd>
+				<dd>“The Headmaster,” JERRY Magazine. Issue 7; October 2013. Web. </dd>
+				<dd>“A Flight of Birds Over Water.” Common Ground Review. Spring/Summer 15.1; August 2013.</dd>
+				<dd>“Every Little Farm Girl Knows How to Fix a Tractor,” New York Tyrant. Vol. 1; August 2006.</dd>
+				<dd>“Face Mongers,” Mudlark Electronic Journal of Poetry & Poetics. Mudlark Flash, No. 15; January 2002.</dd>
+				<dd></dd>
+				<dd>Regular contributor at Microcosmos, a multimedia blog of microbiology: https://microcosmos.foldscope.com/?author=739/</dd>
+
+				<dt>TEACHING EXPERIENCE</dt>
+				<dd><strong>Writing Consultant</strong></dd>
+				<ul>
+					<li>Baruch College Writing Center, New York, NY (2014-2016)</li>
+					<li>Tutored undergraduate and graduate students at CUNY’s business school</li>
+					<li>Collaboratively researched questions of social justice, international student learning, academic culture, and visual learning with colleagues in semester-long inquiry groups.</li>
+				</ul>
+				<dd></dd>
+				<dd>Columbia University Writing Center, New York, NY (2011-2016)</dd>
+				<ul>
+					<li>Tutored undergraduate and graduate students, as well as faculty across the full range of the university departments</<li>
+					<li>Mentored incoming consultants each year at new consultant orientation and in the course of the semester</li>
+					<li>Designed and led staff development workshops on visual learning, ESL and international writers, effective note-taking strategies, and status</li>
+					<li>Helped writing center coordinator with designing a dissertation writer’s boot camp for the spring 2016 semester</li>
+				</ul>
+				<dd></dd>
+				<dd><strong>Lecturer</strong></dd>
+				<dd>Fordham University, New York, NY (2012-2015)</dd>
+				<ul>
+					<li>Designed and taught courses in composition and literature</li>
+					<li>Designed a course in reading comics as literature</li>
+				</ul>
+				<dd></dd>
+				<dd>Globe Institute of Technology, New York, NY (2011-2013)</dd>
+				<ul>
+					<li>Taught courses in writing, literature, and speech</li>
+					<li>Collaboratively designed a progressive composition program for the school</li>
+				</ul>
+				<dd></dd>
+				<dd><strong>Creative Writing Instructor</strong></dd>
+				<dd>Columbia University Summer Program, New York, NY (2008-present)</dd>
+				<ul>
+					<li>Taught small group workshops to senior-level high school students in a three-week intensive summer course</li>
+					<li>Developed a morning seminar to introduce younger high school students to new kinds of creative thinking and approaches to writing in tandem with lessons in workshop.</li>
+				</ul>
+				<dd></dd>
+				<dd>Center for Talent Development (CTD), Northwestern University, Evanston, IL (2009-2010)</dd>
+				<ul>
+					<li>Taught three-week, full day summer seminar and workshop to high school students across a full range of learning</li>
+					<li>Designed syllabus and selected appropriate readings to develop students’ understanding of narrative, poetics, and playwriting.</li>
+				</ul>
+
+				<dt>COURSES TAUGHT</dt>
+				<dd>Composition I and II</dd>
+				<dd>Texts and Contexts: The Short Story</dd>
+				<dd>Texts and Contexts: Art of the Graphic Novel</dd>
+				<dd>Introduction to Literature </dd>
+				<dd>Creative Writing Workshop</dd>
+				<dd>Creative Writing Seminar</dd>
+				<dd>Public Speaking</dd>
+
+				<dt>ADMINISTRATIVE EXPERIENCE</dt>
+				<dd><strong>Coordinator, Columbia Artists Teachers Program, New York, NY (2009-2010)</strong></dd>
+				<dd></dd>
+				<dd><strong>Codirector, Columbia Artists Teachers Program, New York, NY (2008-2009)</strong></dd>
+				<dd></dd>
+				<dd><strong>Founder and Artistic Associate, Tantalus Theater Group, Chicago, IL (2003-2007)</strong></dd>
+				<ul>
+					<li>Coauthored and produced site-specific and guerrilla theater pieces dealing with ritual and culture</li>
+				</ul>
+
+				<dt>FREELANCE EXPERIENCE</dt>
+				<dd><strong>Editor and Writer, One Month, New York, NY (2015)</strong></dd>
+				<ul>
+					<li>Wrote and edited blog posts on relevant subjects of interest to an educational startup for entrepreneurs</li>
+					<li>Researched and interviewed entrepreneurs for the blog</li>
+					<li>Consulted on pedagogical strategies for One Month’s online Learning Library</li>
+				</ul>
+				<dd></dd>
+				<dd><strong>Graphic Novels Reviewer, Publisher’s Weekly, New York, NY (2011-2013)</strong></dd>
+
+				<dt>AWARDS AND FELLOWSHIPS</dt>
+				<dd>Playa Artist Residency, Summerlake, Oregon, November 2016</dd>
+				<dd>Robert J. Connors Memorial Scholarship, Northeastern Writing Center Association, March 2014.</dd>
+				<dd>Columbia Artists/Teachers Teaching Fellowship, Columbia University School of the Arts, 2007-2010.</dd>
+				<dd>Writing Program Fellowship, Columbia University School of the Arts 2007-2009.</dd>
+				<dd>Francis Hulme Play Writing Award, UNCA 1999.</dd>
+
+				<dt>PROFESSIONAL ACTIVITY</dt>
+				<dd><strong>Script Developer, Sharp+Weird (February 2016)</strong></dd>
+				<ul>
+					<li>Collaboratively designed “Egg Hunt” an interactive fiction game playable through text messaging and e-mail</li>
+					<li>Developed script trees for the game</li>
+				</ul>
+
+
+				<dd><strong>Super User, Microcosmos Blog, Stanford University (Sept 2015-Present)</strong></dd>
+				<ul>
+					<li>Regularly post essays on microbiology and urban spaces</li>
+					<li>Engage with community of scientists and educators on effectively using science in classrooms</li>
+				</ul>
+
+				<dd><strong>Workshop Presenter (April 2016)</strong></dd>
+				<dd>Northeast Writing Center Association Conference, Keene, NH</dd>
+				<ul>
+					<li>Presenting “Take Note” an interactive workshop on note taking strategies</li>
+				</ul>
+
+				<dd><strong>Conference Presentation (October 2015)</strong></dd>
+				<dd>International Writing Center Association Conference, Pittsburgh, PA</dd>
+				<ul>
+					<li>Presenting “Grammars of Design, Making, and Motion in the Writing Center”</li>
+				</ul>
+
+
+				<dd><strong>Translation Reading (October 2014)</strong></dd>
+				<dd>Exchange Rates: The Bushwick International Art Expo, Brooklyn, NY</dd>
+				<ul>
+					<li>Panel member in “Renderings: Encounters and Translations,” discussion of artists and writers on translations</li>
+				</ul>
+
+				<dd><strong>Conference Presentation (March 2014)</strong></dd>
+				<dd>Northeast Writing Center Association Conference, Providence, RI</dd>
+				<ul>
+					<li>Presenting “Use Your Doodle,” on translating visual grammars in writing center practice</li>
+				</ul>
+
+				<dd><strong>Guest Lecture (April 2013)</strong></dd>
+				<dd>Pocket Utopia Gallery, New York, NY</dd>
+				<ul>
+					<li>Gallery talk on narrative and visual art to accompany Paul D’Agostino’s “Twilit Ensembles”</li>
+				</ul>
+
+				<dd><strong>Columbia: A Journal of Literature and Art, New York, NY (2008-2009)</strong></dd>
+				<dd>Fiction Reader</dd>
+
+				<dt>EDUCATION</dt>
+				<dd>MFA, Fiction Writing, Columbia University, 2010</dd>
+				<dd>BA, Theater, University of North Carolina at Asheville, 2000</dd>
+
+			</dl>			
+
+		</div>
+	</div> <!-- end row -->	
+
+</div> <!--end container-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  </body>
+</html>
