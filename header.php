4640bd52245958448817fec18c8efca47488eb62
Alex Miles Younger <alex@alxpck.com>
2017-01-29 20:07:52 -0500

    update favicon

--- header.php
+++ header.php
@@ -7,15 +7,13 @@
     <title><?php echo $title ?> | Matthew Rossi</title>
 	
 	<!-- Favicon -->
-	<link rel="icon" type="image/ico" href="https://mlrossi.com/img/favicon/favicon.ico">
-
-	<link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" href="/img/favicon/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="/img/favicon/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="/img/favicon/manifest.json">
-	<link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-	<link rel="shortcut icon" href="/img/favicon/favicon.ico">
-	<meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="favicon/manifest.json">
+	<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="favicon/favicon.ico">
+	<meta name="msapplication-config" content="favicon/browserconfig.xml">
 	<meta name="theme-color" content="#ffffff">
 	
 	<!-- Google Font: Playfair -->
